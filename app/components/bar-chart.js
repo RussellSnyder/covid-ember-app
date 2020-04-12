2ee26ab914bc805a63a33b011221f356cb2d7cbc
Russell Snyder <russellsnyder@Russells-MBP.fritz.box>
2020-04-12 20:16:31 +0200

    added barcharts

--- app/components/bar-chart.js
+++ app/components/bar-chart.js
@@ -2,7 +2,14 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { select } from 'd3-selection'
 import { scaleLinear, scaleBand } from 'd3-scale'
-import { maxBy } from 'lodash';
+import { axisBottom, axisLeft } from 'd3-axis';
+import { maxBy, uniqueId, sortBy, reverse } from 'lodash';
+
+const COLOR_MAP = {
+  deaths: 'crimson',
+  confirmed: 'brown',
+  recovered: 'green'
+}
 
 export default class BarChartComponent extends Component {
   @action
@@ -23,9 +30,27 @@ export default class BarChartComponent extends Component {
     // get rid of null / undefined
     data = data.filter(Boolean);
 
+    // just for testing
+    if (this.args.threshold) {
+      data = data.filter(data => data.deaths > this.args.threshold);
+    }
+
+    // sort by compareBy
+    data = reverse(sortBy(data, compareBy))
+
+
+    // add ids for more complex functionality
+    data = data.map(data => ({
+      id: `${data.name.split(' ').join().toLowerCase()}-${uniqueId()}`,
+      ...data
+    }));
+
     const svg = select(element)
     svg.height = parseInt(svg.style('height'))
     svg.width = parseInt(svg.style('width'))
+    svg.margin = {
+      bottom: 100
+    }
 
     let yScale = scaleLinear()
       .domain([0, maxBy(data, compareBy)[compareBy]])
@@ -39,9 +64,42 @@ export default class BarChartComponent extends Component {
     svg.selectAll('rect').data(data)
       .enter()
       .append('rect')
+      .attr('id', (data) => `rect-${data.id}`)  
+      .attr('fill', COLOR_MAP[this.args.compareBy])
       .attr('width', `${xScale.bandwidth()}%`)  
       .attr('height', (data) => `${yScale(data[compareBy])}%`)
       .attr('x', (data) => `${xScale(data.name)}%`)
       .attr('y', (data) => `${100 - yScale(data[compareBy])}%`)
-  }
+
+      svg.selectAll('svg').data(data)
+      .enter()
+      .append('svg')
+      .attr('class', 'country-text')
+      // .attr('fill', 'none')
+      .attr('width', `${xScale.bandwidth()}%`)  
+      .attr('height', (data) => `${yScale(data[compareBy])}%`)
+      .attr('x', (data) => `${xScale(data.name)}%`)
+      .attr('y', (data) => `${svg.height - 90}px`)
+      .append('text')
+      .attr('transform', 'rotate(90)')
+      .attr('alignment-baseline', "middle")
+      .html(data => `${data.name}`)
+      .attr('y', `-${svg.width * xScale.bandwidth() / 100 / 2}px`)
+
+    // left axis
+      let leftAxisScale = scaleLinear()
+      .domain([0, maxBy(data, compareBy)[compareBy]])
+      .range([svg.height - svg.margin.bottom, 0])
+
+      svg.append("g")
+        .call(axisLeft(leftAxisScale))
+        .attr("y", -15)
+
+      svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -75)
+        .attr("x",0 - (svg.height / 2))
+        // .attr("dy", "1em")
+        .style("text-anchor", "center")
+        .text(compareBy);            }
 }
