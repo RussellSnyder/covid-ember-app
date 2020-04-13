048fd8e4b136101526ec990143e5bc50edcb4a8c
Russell Snyder <russellsnyder@Russells-MBP.fritz.box>
2020-04-13 16:26:50 +0200

    remove adapter and prepend fetch calls

--- app/routes/country.js
+++ app/routes/country.js
@@ -1,19 +1,13 @@
 import Route from '@ember/routing/route';
 import fetch from 'fetch';
-import { action } from '@ember/object';
+import config from '../../config/environment'
 
 export default class CountryRoute extends Route {
   async model(params) {
     const { country_name: countryName } = params
 
-    const response = await fetch(`/api/country/${countryName}`);
+    const response = await fetch(`${config.API_ENPOINT}/api/country/${countryName}`);
     const data = await response.json();
     return { data, countryName }
-  }
-
-  @action
-  activate() {
-    // console.log('activate refresh')
-    // this.refresh()
   }
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,10 @@
 import Route from '@ember/routing/route';
 import fetch from 'fetch';
+import config from '../../config/environment'
 
 export default class IndexRoute extends Route {
   model() {
-    return fetch('/api/countries/').then(function(response) {
+    return fetch(`${config.API_ENPOINT}/api/countries/`).then(function(response) {
       return response.json();
     });
   }
