8c8da5a47b412b89667f6beaa32d700e264eba84
Russell Snyder <russellsnyder@Russells-MBP.fritz.box>
2020-04-13 16:10:44 +0200

    fix syntax error

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import config from '../../config/environment'
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
-  host: config.API_ENPOINT
+  host = config.API_ENPOINT
 }
