4c9ef121cc00731e3d4e3b1d31714358469c9aca
Russell Snyder <russellsnyder@Russells-MBP.fritz.box>
2020-04-13 16:20:08 +0200

    change from json to rest adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
-import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import RESTAdapter from '@ember-data/adapter/rest';
 import config from '../../config/environment'
 
-export default class ApplicationAdapter extends JSONAPIAdapter {
+export default class ApplicationAdapter extends RESTAdapter {
   host = config.API_ENPOINT
 }
